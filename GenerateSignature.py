23d675cb71b1e1b5c0a8871dc51063e0b3bcd4c5
MrIzzat <guineapigsarecute3748@gmail.com>
2023-10-22 20:23:19 +0300

    Improved Signature Generator

--- GenerateSignature.py
+++ GenerateSignature.py
@@ -2,23 +2,11 @@ import collections as ct
 
 
 def generateSignature(message):
-    lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
-    capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numbers = "0123456789"
-    whiteSpace = " "
-    puncuation = ",./;!?"
-    specialCharacters = "@#$%^&*()_+-=/*<>[]{}~`"
+    signature = 0
+    for letter in message:
+        signature += ord(letter) ** 2
 
-    lowerCaseLettersCounter = sum(v for k, v in ct.Counter(message).items() if k in lowerCaseLetters)
-    capitalLettersCounter = sum(v for k, v in ct.Counter(message).items() if k in capitalLetters)
-    numbersCounter = sum(v for k, v in ct.Counter(message).items() if k in numbers)
-    whiteSpaceCount = sum(v for k, v in ct.Counter(message).items() if k in whiteSpace)
-    puncuationCount = sum(v for k, v in ct.Counter(message).items() if k in puncuation)
-    specialCharactersCount = sum(v for k, v in ct.Counter(message).items() if k in specialCharacters)
-
-    signatureInteger = lowerCaseLettersCounter + 2 * capitalLettersCounter + 3 * numbersCounter + whiteSpaceCount + 3 * puncuationCount + 4 * specialCharactersCount
-
-    signatureHex = hex(signatureInteger)
-    signatureHex = signatureHex[2:]  # remove the 0x from the begining
+    signatureHex = hex(signature)
+    signatureHex = signatureHex[2:]
 
     return signatureHex
